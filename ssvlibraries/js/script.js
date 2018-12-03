e27ad427ba6cbca0182bb6663fe6e8a7115810de
Bien Vu <tienbienit1992@gmail.com>
2018-12-03 10:35:46 +0700

    SSV: add base style

---
+++ ssvlibraries/js/script.js
@@ -0,0 +1,21 @@
+/*jslint browser: true*/
+/*global $, jQuery, Modernizr, enquire*/
+(function (window, document, $) {
+  var $html = $('html'),
+    mobileOnly = "screen and (max-width:47.9375em)", // 767px.
+    mobileLandscape = "(min-width:30em)", // 480px.
+    tablet = "(min-width:48em)"; // 768px.
+  // Add  functionality here.
+
+  // Table responsive
+  Drupal.behaviors.tableResponsive = {
+    attach: function (context, settings) {
+      var $table = $('table', context);
+      if ($table.length &&
+        !$table.parent().hasClass('table-responsive')) {
+        $table.not($table.find('table')).wrap('<div class="table-responsive"></div>');
+      }
+    }
+  };
+
+}(this, this.document, this.jQuery));
