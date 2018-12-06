68aae8320c2acb4cde006caf637e7c2bd80ee640
thanhtung2705 <thanhtung1996fsd@gmail.com>
2018-12-06 10:43:38 +0700

    ssvlib: update box-text

--- ssvlibraries/js/script.js
+++ ssvlibraries/js/script.js
@@ -18,4 +18,15 @@
     }
   };
 
+  $('.js-read-more').click(function(e) {
+    e.preventDefault();
+    if($('.read-more').hasClass('is-show')) {
+      $('.read-more').removeClass('is-show');
+      $(this).text('READ MORE >');
+    }else {
+      $('.read-more').addClass('is-show');
+      $(this).text('READ LESS >');
+    }
+  });
+
 }(this, this.document, this.jQuery));
