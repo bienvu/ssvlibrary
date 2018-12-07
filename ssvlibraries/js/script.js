835e37ff35952a4c1ebb3cb9f8b73deba1adf02f
thanhtung2705 <thanhtung1996fsd@gmail.com>
2018-12-07 10:34:11 +0700

    ssvlib: update

--- ssvlibraries/js/script.js
+++ ssvlibraries/js/script.js
@@ -7,6 +7,26 @@
     tablet = "(min-width:48em)"; // 768px.
   // Add  functionality here.
 
+   //slick for box-hero
+    $('.js-slider').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      dots: true,
+      fade: true,
+      responsive: [{
+        breakpoint: 1024,
+        settings: {
+          dots: false,
+          arrows: false
+    });
+      
+    $('a[data-slide]').click(function(e) {
+   e.preventDefault();
+   var slideno = $(this).data('slide');
+   $('.gim1__wrap').slick('slickGoTo', slideno - 1);
+
   // Table responsive
   Drupal.behaviors.tableResponsive = {
     attach: function (context, settings) {
